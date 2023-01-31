30bb4dffc6d70c77992adec372e0ee305141f5ea
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-01-31 16:40:59 +0800

    update(boot): protected mode (impl gdt)

--- boot.asm
+++ boot.asm
@@ -1,5 +1,9 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start:
   jmp short start
   nop
@@ -7,58 +11,69 @@ _start:
 times 33 db 0
 
 start:
-  jmp 0x7c0:step2
+  jmp 0:step2
 
 step2:
   cli  ; Clear Interrupts
-  mov ax, 0x7c0
+  mov ax, 0x00
   mov ds, ax
   mov es, ax
-  mov ax, 0x00
   mov ss, ax
   mov sp, 0x7c00
   sti  ; Enables Interrupts
 
-  mov ah, 2 ; read sctor command
-  mov al, 1 ; one sctor to read
-  mov ch, 0 ; cylinder low eight bits
-  mov cl, 2 ; read sector two
-  mov dh, 0 ; head number
-  mov bx, buffer
-  int 0x13
-  jc error
+.load_protected:
+  cli
+  lgdt[gdt_descriptor]
+  mov eax, cr0
+  or eax, 0x1
+  mov cr0, eax
+  jmp CODE_SEG:load32
 
-  mov si, buffer
-  call print
+; GDT: https://wiki.osdev.org/Global_Descriptor_Table
+gdt_start:
+gdt_null:
+  dd 0x0
+  dd 0x0
+
+; offset 0x8
+gdt_code:     ; cs should point to this
+  dw 0xffff   ; segment limit first 0-15 bits
+  dw 0        ; base first 0-15 bits
+  db 0 ; base
+  db 0x9a
+  db 11001111b
+  db 0
+
+; offset 0x10
+gdt_data: ; ds, ss, es, fs, gs
+  dw 0xffff   ; segment limit first 0-15 bits
+  dw 0        ; base first 0-15 bits
+  db 0 ; base
+  db 0x92
+  db 11001111b
+  db 0
+
+gdt_end:
+
+gdt_descriptor:
+  dw gdt_end - gdt_start - 1 ; gdt size
+  dd gdt_start
+
+[BITS 32]
+load32:
+  mov ax, DATA_SEG
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  mov ebp, 0x00200000
+  mov esp , ebp
   jmp $
-
-error:
-  mov si, error_message
-  call print
-  jmp $
-
-print:
-  mov bx, 0
-.loop:
-  lodsb
-  cmp al, 0
-  je .done
-  call print_char
-  jmp .loop
-.done:
-  ret
-
-print_char:
-  mov ah, 0eh
-  int 0x10   ; Bios sys call
-  ret
-
-error_message: db "Fail to load sector", 0
 
 ; Padding zero
 times 510-($ - $$) db 0
 dw 0xAA55
 
 buffer:
-; nasm -f bin -o boot.bin ./boot.asm
-; qemu-system-x86_64 -nographic -hda ./boot.bin
