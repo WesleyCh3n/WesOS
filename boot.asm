c7d7a38575de1b12cbcd95002e5e707aafae7ef6
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-01-31 15:37:36 +0800

    update(boot): output "Hello World"

--- boot.asm
+++ boot.asm
@@ -2,15 +2,31 @@ ORG 0x7c00
 BITS 16
 
 start:
-  mov ah, 0eh
-  mov al, 'A'
-  mov bx, 0
-  int 0x10
-
+  mov si, message
+  call print
   jmp $
 
-padding_zero times 510-($ - $$) db 0
-msg dw 0xAA55
+print:
+  mov bx, 0
+.loop:
+  lodsb
+  cmp al, 0
+  je .done
+  call print_chaar
+  jmp .loop
+.done:
+  ret
+
+print_char:
+  mov ah, 0eh
+  int 0x10   ;bios sys call
+  ret
+
+message: db "Hello World!", 0
+
+; Padding zero
+times 510-($ - $$) db 0
+dw 0xAA55
 
 ; nasm -f bin -o boot.bin ./boot.asm
 ; qemu-system-x86_64 -nographic -hda ./boot.bin
